5555da726c2882ba4e7beff912311d4a94343d87
steventrinh123 <131470079+steventrinh123@users.noreply.github.com>
2024-04-20 23:01:18 -0700

    Add files via upload
    commit files

---
+++ ProjectH/core/src/com/mygdx/game/ProjectH.java
@@ -0,0 +1,25 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class ProjectH extends ApplicationAdapter {
+
+	
+	@Override
+	public void create () {
+
+	}
+
+	@Override
+	public void render () {
+
+	}
+	
+	@Override
+	public void dispose () {
+
+	}
+}
