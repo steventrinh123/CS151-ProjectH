a1f7bcdb76a26d57942804067dc5689e2e136a72
steventrinh123 <steventrinh1212@gmail.com>
2024-04-25 15:41:07 -0700

    created/implemented buttons

--- ProjectH/core/src/Screens/MenuScreen.java
+++ ProjectH/core/src/Screens/MenuScreen.java
@@ -4,15 +4,20 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.game.ProjectH;
 
 public class MenuScreen implements Screen {
     private ProjectH game;
     private OrthographicCamera camera;
 
+
     public MenuScreen(ProjectH game) {
         this.game = game;
+
     }
 
     @Override
@@ -21,7 +26,8 @@ public class MenuScreen implements Screen {
     }
     @Override
     public void render(float delta) {
-
+        Gdx.gl.glClearColor(0, 150, 5, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
     }
 
     @Override
